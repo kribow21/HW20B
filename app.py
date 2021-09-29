7717d1271e230b7141c355cb35378b53afb9efe4
kribow21 <kristininnotech@gmail.com>
2021-09-28 22:12:41 -0600

    fixed the login query and logic learned how to get a thing in a tuple for the user_id

--- app.py
+++ app.py
@@ -8,36 +8,36 @@ def login_hacker():
     print("Please Login")
     alias_name = input("Enter username:")
     user_password = input("Enter password:")
-    cursor.execute("SELECT * FROM hackers WHERE alias=?, password=?",[alias_name,user_password,])
+    cursor.execute("SELECT * FROM hackers WHERE alias=? AND password=?",[alias_name,user_password,])
     hacker_login = cursor.fetchone()
     print(hacker_login)
-    if(hacker_login.length != 0):
+    if(hacker_login != None):
         print('sucess login')
-        hacker_choices()
+        hacker_choices(hacker_login)
     else:
         print('failed to login')
 
-def hacker_choices():
+def hacker_choices(hacker_login):
     print('Select an option:')
     print('1: Enter a new exploit')
     print('2: See all your exploits')
     print('3: See all exploits by others')
     print('4: Exit application')
     hacker_choice = input()
-    while hacker_choice.length == 1:
+    while True:
         if(hacker_choice == '1'):
             print('Enter new exploit:')
             exploit = input()
-            cursor.execute("INSERT INTO exploits(content) VALUES (?)",[exploit])
+            cursor.execute("INSERT INTO exploits(content,user_id) VALUES (?,?)",[exploit,hacker_login[0]])
             conn.commit()
             print('exploit created')
         elif(hacker_choice == '2'):
-            cursor.execute("SELECT * FROM exploits WHERE user_id=")
+            cursor.execute("SELECT * FROM exploits WHERE user_id=?",[hacker_login[0]])
             exploit_list = cursor.fetchall()
             for ex in exploit_list:
                 print(ex)
         elif(hacker_choice == '3'):
-            cursor.execute("SELECT * FROM exploits WHERE user_id!=")
+            cursor.execute("SELECT * FROM exploits WHERE user_id != ?",[hacker_login[0]])
             others_exploits = cursor.fetchall()
             for others in others_exploits:
                 print(others)
