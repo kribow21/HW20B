7f3b8a581f44f2f9e35dd2cd0fa890cb2a42d374
kribow21 <kristininnotech@gmail.com>
2021-09-29 18:03:54 -0600

    added db dump

---
+++ cliSocialMedia copy.sql
@@ -0,0 +1,79 @@
+-- MariaDB dump 10.19  Distrib 10.4.21-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: cli_social_media
+-- ------------------------------------------------------
+-- Server version	10.4.21-MariaDB-1:10.4.21+maria~stretch
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `exploits`
+--
+
+DROP TABLE IF EXISTS `exploits`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `exploits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `exploits_FK` (`user_id`),
+  CONSTRAINT `exploits_FK` FOREIGN KEY (`user_id`) REFERENCES `hackers` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exploits`
+--
+
+LOCK TABLES `exploits` WRITE;
+/*!40000 ALTER TABLE `exploits` DISABLE KEYS */;
+INSERT INTO `exploits` VALUES (1,'to see my kids grades ',5),(2,'i need money asap',2),(3,'did it for the shoes',4),(4,'cause im evil',2),(5,'hehehehe',5),(6,'muahhaahaha',1);
+/*!40000 ALTER TABLE `exploits` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hackers`
+--
+
+DROP TABLE IF EXISTS `hackers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hackers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `alias` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hackers`
+--
+
+LOCK TABLES `hackers` WRITE;
+/*!40000 ALTER TABLE `hackers` DISABLE KEYS */;
+INSERT INTO `hackers` VALUES (1,'John David','besthackerever'),(2,'Ryan Smith','awesomehacker'),(3,'Michael James','theoneandonly'),(4,'Tina Trump','hackerlife'),(5,'Sam Stad','secrethacker');
+/*!40000 ALTER TABLE `hackers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-09-29 23:52:57
